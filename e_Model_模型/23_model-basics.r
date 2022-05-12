884f1da5d8916c976dde8388f6f6ea1100bf090a
cworld1 <cworld0@qq.com>
2022-05-12 23:45:30 +0800

    docs(model): update 23-model-basics doc

--- e_Model_模型/23_model-basics.r
+++ e_Model_模型/23_model-basics.r
@@ -41,7 +41,7 @@ model1(c(7, 1.5), sim1)
 # 所以我们新建了函数来获得方差
 measure_distance <- function(mod, data) {
     diff <- data$y - model1(mod, data) # diff 为 data 中实际 y 值与 model1 得到的理想 y 值的差
-    sqrt(mean(diff^2)) # 返回所有 diff 平方的均值进行的开放
+    sqrt(mean(diff^2)) # 返回所有 diff 平方的均值进行的开方（方差）
 }
 # 试图用 a1 = 7 和 a2 = 1.5 数据求得关于 sim1 的方差
 measure_distance(c(7, 1.5), sim1)
@@ -71,13 +71,64 @@ ggplot(sim1, aes(x, y)) +
     geom_point(size = 2, colour = "grey30") +
     geom_abline(
         aes(intercept = a1, slope = a2, colour = -dist), # 由 dist 反向上色，即越精确颜色越鲜亮
-        data = filter(models, rank(dist) <= 10) # 不是所有线条都需要显示，这里选择 dist <= 10
+        data = filter(models, rank(dist) <= 10) # 不是所有线条都需要显示，这里选择 dist 排名前 10
     )
 
+# 将 a1 和 a2 用图表示出来（同样上色），这里选择 dist 排名前 10来高亮显示（叠加）
 ggplot(models, aes(a1, a2)) +
     geom_point(
-        data = filter(models, rank(dist) <= 10),
-        size = 4,
-        colour = "red"
+        data = filter(models, rank(dist) <= 10), # 先画高亮部分
+        size = 4, colour = "red"
     ) +
     geom_point(aes(colour = -dist))
+
+# 事实上除了用 runif 创建随机模型外，我们还常常使用标准步长的规整矩阵数据模型来验算已知的回归模型
+grid_models <- expand.grid(
+    # expand.grid 用于于快速创建数据集（data frame）
+    a1 = seq(-5, 20, length = 25), # seq 用于快速生成序列，参数分别代表起始、结束和序列长
+    a2 = seq(1, 3, length = 25)
+) %>%
+    mutate(dist = purrr::map2_dbl(a1, a2, sim1_dist))
+ggplot(grid_models, aes(a1, a2)) +
+    geom_point(
+        data = filter(grid_models, rank(dist) <= 10),
+        size = 4, colour = "red"
+    ) +
+    geom_point(aes(colour = -dist))
+# 同样，我们把拟合出的回归模型放到原始数据集对照，事实告诉我们拟合的效果非常好
+ggplot(sim1, aes(x, y)) +
+    geom_point(size = 2, colour = "grey30") +
+    geom_abline(
+        aes(intercept = a1, slope = a2, colour = -dist),
+        data = filter(grid_models, rank(dist) <= 10)
+    )
+
+# 当然这对 a1 和 a2 数据是我们根据矩阵数据集拟合得到的。我们现在可以通过大致拟合度，继续细化数据
+# 这样已知细化下去，我们终究得到拟合度极高的数据。
+# 所以我们需要一个名为牛顿-拉夫森搜索的数字最小化工具。
+# 牛顿-拉夫森工具的思想很简单：你选择一个起点，环顾四周最陡峭的斜坡。
+# 然后，你沿着斜坡滑了一点，然后一遍又一遍地重复，直到你不能再低了。
+# 在 R 中，我们可以使用 optim() 做到这一点：
+best <- optim(
+    par = c(0, 0), # 要优化的参数的初始值
+    fn = measure_distance, # 要最小化（或最大化）的函数，所以 par 参数是要进行最小化的参数向量。它应该返回标量结果
+    data = sim1
+)
+# 注意 optm() 将返回一个 list，其中：
+# par 为找到的最佳参数向量，value 为参数向量对应的返回值
+best$par
+#> [1] 4.222248 2.051204
+# 我们画图验证一下刚刚得到的 a1 和 a2。相应的，它的拟合程度也相当完美
+ggplot(sim1, aes(x, y)) +
+    geom_point(size = 2, colour = "grey30") +
+    geom_abline(intercept = best$par[1], slope = best$par[2], colour = "blue")
+
+# 线性模型的一般形式为 y = a_1 + a_2 * x_1 + a_3 * x_2 + ... + a_n * x_(n - 1)。
+# 在 R 里有一个专门为拟合线性模型而设计的工具 lm()。
+# lm() 用一种特殊的方式来指定模型，即公式。
+# 公式例如 y ~ x，lm() 将自动转换为 y = a_1 + a_2 * x 这样的函数
+sim1_mod <- lm(y ~ x, data = sim1)
+coef(sim1_mod) # coef() 是一个通用函数，用于从建模函数返回的对象中提取模型系数
+# 结果与预期完全一致
+#> (Intercept)           x
+#>    4.220822    2.051533
